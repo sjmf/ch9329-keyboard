38f8b99b4e0ca66aade56b45041169b32741cf66
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2025-06-08 21:24:22 +0100

    Allow CaptureDevice to run on main thread due to crash on OSX.
    Related: https://github.com/opencv/opencv/issues/22602

--- backend/video.py
+++ backend/video.py
@@ -1,30 +1,46 @@
 #!/usr/bin/env python
 import cv2
-import sys
 import numpy
 import threading
+import logging
+
+logger = logging.getLogger(__name__)
 
 CAMERAS_TO_CHECK = 4
 
+class CaptureDeviceException(Exception):
+    pass
+
 class CaptureDevice:
-    def __init__(self, fullscreen=False):
+    def __init__(self, fullscreen=False, threaded=False):
         self.cam = None
         self.fullscreen = fullscreen
         self.running = False
-        self.thread = threading.Thread(target=self.capture)
+        if threaded:
+            self.thread = threading.Thread(target=self.capture)
+        else:
+            self.thread = None
 
     def run(self):
+        if not isinstance(self.thread, threading.Thread):
+            raise CaptureDeviceException("Capture device not running in thread")
+        
         self.thread.start()
         self.thread.join()
 
     def start(self):
+        if not isinstance(self.thread, threading.Thread):
+            raise CaptureDeviceException("Capture device not running in thread")
+        
         self.thread.start()
 
     def stop(self):
         self.running = False
-        self.thread.join()
-    
-    def cameraIndices(self):
+
+        if isinstance(self.thread, threading.Thread):
+            self.thread.join()
+
+    def _cameraIndices(self):
         cameras = []
         # check for cameras
         for index in range(0, CAMERAS_TO_CHECK):
@@ -45,8 +61,12 @@ class CaptureDevice:
         return cameras
 
     def capture(self, camIndex=0, exitKey=27):
-        cameras = self.cameraIndices()
-        print(f"Found {len(cameras)} cameras.")
+        logger.info(f"Starting video in {"fullscreen" if self.fullscreen else "window"}...")
+
+        cameras = self._cameraIndices()
+        logger.info(f"Found {len(cameras)} cameras.")
+        logger.debug(cameras)
+
         props = cameras[camIndex]
 
         cv2.namedWindow("kvm", cv2.WINDOW_NORMAL)
@@ -64,15 +84,17 @@ class CaptureDevice:
                 cv2.imshow('kvm', frame)
 
                 # Default is 'ESC' to exit the loop
+                # 50 = 20fps?
                 if cv2.waitKey(50) == exitKey or not self.running:
                     cam.release()
         except cv2.error as e:
-            print(e, file=sys.stderr)
+            logger.error(e)
         finally:
             self.running = False
 
-        # Release the capture and writer objects
-        cv2.destroyWindow("kvm")
+            # Release the capture and writer objects
+            logger.info("Camera released. Destroying video window...")
+            cv2.destroyWindow("kvm")
 
 if __name__ == "__main__":
     cap = CaptureDevice(fullscreen=False)
