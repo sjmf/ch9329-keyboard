ecbaedfde8a93ec21db9f014e31f59f11f4ac1de
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2025-06-12 23:17:20 +0100

    Populate dropdowns after startup, to avoid GUI start lag

--- kvm_serial/kvm.py
+++ kvm_serial/kvm.py
@@ -9,10 +9,12 @@ from tkinter import ttk, messagebox
 
 try:
     from kvm_serial.utils.communication import list_serial_ports
+    from kvm_serial.backend.video import CameraProperties, CaptureDevice
 except ModuleNotFoundError:
     # Allow running as a script directly
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
     from utils.communication import list_serial_ports
+    from backend.video import CameraProperties, CaptureDevice
 
 logger = logging.getLogger(__name__)
 
@@ -33,6 +35,7 @@ class KVMGui(tk.Tk):
         mouse_var (tk.BooleanVar): State of the mouse checkbox.
         kb_backend_var (tk.StringVar): Selected keyboard backend.
         serial_port_var (tk.StringVar): Selected serial port.
+        video_device_var (tk.StringVar): Selected video device.
         baud_rate_var (tk.IntVar): Selected baud rate.
         window_var (tk.BooleanVar): State of the windowed mode checkbox.
         verbose_var (tk.BooleanVar): State of the verbose logging checkbox.
@@ -50,13 +53,16 @@ class KVMGui(tk.Tk):
     """
 
     kb_backends: list[str]
-    serial_ports: list[str]
     baud_rates: list[int]
+    serial_ports: list[str]
+    video_devices: list[CameraProperties]
+
     keyboard_var: tk.BooleanVar
     video_var: tk.BooleanVar
     mouse_var: tk.BooleanVar
     kb_backend_var: tk.StringVar
     serial_port_var: tk.StringVar
+    video_device_var: tk.StringVar
     baud_rate_var: tk.IntVar
     window_var: tk.BooleanVar
     verbose_var: tk.BooleanVar
@@ -70,13 +76,9 @@ class KVMGui(tk.Tk):
 
         # Dropdown values
         self.kb_backends = ["pynput", "curses", "tty", "usb"]
-        self.serial_ports = list_serial_ports()
         self.baud_rates = [1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200]
-
-        if len(self.serial_ports) == 0:
-            self.destroy()
-            messagebox.showerror("Start-up Error!", "No serial ports found. Exiting.")
-            return
+        self.serial_ports = []
+        self.video_devices = []
         
         # Window characteristics
         self.title("Python KVM")
@@ -87,7 +89,8 @@ class KVMGui(tk.Tk):
         self.video_var = tk.BooleanVar(value=True)
         self.mouse_var = tk.BooleanVar(value=True)
         self.kb_backend_var = tk.StringVar(value=self.kb_backends[0])
-        self.serial_port_var = tk.StringVar(value=self.serial_ports[-1])
+        self.serial_port_var = tk.StringVar(value="Loading serial...")
+        self.video_device_var = tk.StringVar(value="Loading cameras...")
         self.baud_rate_var = tk.IntVar(value=self.baud_rates[3])
         self.window_var = tk.BooleanVar(value=False)
         self.verbose_var = tk.BooleanVar(value=False)
@@ -95,9 +98,9 @@ class KVMGui(tk.Tk):
         # Title row
         tk.Label(self, text="KVM Controller", font=(TYPEFACE, 14, "bold")).grid(row=0, column=0, padx=(10, 10), pady=(10, 10))
         
-        # Vertical bar between columns 0 and 1, spanning rows 1-3
-        bar = tk.Frame(self, width=1, height=100, bg="gray")
-        bar.grid(row=1, column=0, rowspan=3, sticky="ne", padx=(0, 10), pady=(10, 0))
+        # Vertical bar between columns 0 and 1, spanning rows 1-4
+        bar = tk.Frame(self, width=1, height=140, bg="gray")
+        bar.grid(row=1, column=0, rowspan=4, sticky="ne", padx=(0, 10), pady=(10, 0))
 
         # Checkboxes
         tk.Checkbutton(self, text="Keyboard", variable=self.keyboard_var).grid(row=1, column=0, sticky="w", padx=10, pady=5)
@@ -117,18 +120,49 @@ class KVMGui(tk.Tk):
 
         # Baud rate dropdown
         tk.Label(self, text="Baud rate").grid(row=3, column=1, sticky="w", padx=10)
-        ttk.Combobox(self, textvariable=self.baud_rate_var, values=[str(rate) for rate in self.baud_rates], state="readonly").grid(row=3, column=2, padx=10, pady=5)
+        self.serial_port_combo = ttk.Combobox(self, textvariable=self.baud_rate_var, values=[], state="readonly")
+        self.serial_port_combo.grid(row=3, column=2, padx=10, pady=5)
+
+        # Video Capture device dropdown
+        tk.Label(self, text="Video Device").grid(row=4, column=1, sticky="w", padx=10)
+        self.video_device_combo = ttk.Combobox(self, textvariable=self.video_device_var, values=[], state="readonly")
+        self.video_device_combo.grid(row=4, column=2, padx=10, pady=5)
 
         # Start and Exit buttons
         self.start_button = tk.Button(self, text="Start", width=15, command=self.on_start, disabledforeground="black")
-        self.start_button.grid(row=4, column=1, pady=20, padx=(0, 5))
-        tk.Button(self, text="Exit", width=15, command=self.on_exit).grid(row=4, column=2, pady=20, padx=(5, 10))
+        self.start_button.grid(row=5, column=1, pady=20, padx=(0, 5))
+        tk.Button(self, text="Exit", width=15, command=self.on_exit).grid(row=5, column=2, pady=20, padx=(5, 10))
 
         # Footer row
         footer = tk.Label(self, text="To exit, press Ctrl + ESC", font=(TYPEFACE, 11), fg="gray")
-        footer.grid(row=5, column=0, columnspan=3, pady=(0, 20))
+        footer.grid(row=6, column=0, columnspan=3, pady=(0, 20))
+
+        # Run deferred tasks
+        self.after(75, self._populateVideoDevices)
+        self.after(500, self._populateSerialPorts)
 
         logging.debug("Initialised Window")
+
+    def _populateSerialPorts(self) -> None:
+        # Populate the serial devices dropdown
+        self.serial_ports = list_serial_ports()
+        logging.debug(self.serial_ports)
+
+        if len(self.serial_ports) == 0:
+            self.destroy()
+            messagebox.showerror("Start-up Error!", "No serial ports found. Exiting.")
+            return
+        
+        self.serial_port_combo["values"] = self.serial_ports
+        self.serial_port_var.set(value=self.serial_ports[-1])
+
+    def _populateVideoDevices(self) -> None:
+        # Populate the video devices dropdown
+        self.video_devices = CaptureDevice.getCameras()
+        logging.info('\n'.join([str(v) for v in self.video_devices]))
+        self.video_device_combo["values"] = [str(v) for v in self.video_devices]
+        if self.video_devices:
+            self.video_device_var.set(str(self.video_devices[0]))
 
     def on_start(self) -> None:
         # Disable Start button and set text to "Running..."
